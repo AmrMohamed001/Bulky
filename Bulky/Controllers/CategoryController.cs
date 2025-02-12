2d4a8909f81ffea13e33d9dac0ec374fb060b09f
amr mohamed <amrmohamd229@gmail.com>
2025-02-12 21:05:19 +0200

    initial commit 🚀

---
+++ Bulky/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Bulky.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Controllers;
+
+public class CategoryController : Controller
+{
+    private AppDbContext db;
+    public CategoryController(AppDbContext ctx)
+    {
+        db = ctx;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        var categories = db.Categories.ToList();
+        return View(categories);
+    }
+    #region Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+        if (int.TryParse(category.Name, out _))
+        {
+            ModelState.AddModelError("Name", "Name must be string");
+            return View(category);
+        }
+        if(category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "Name must Not equal order display");
+            return View(category);
+        }
+        db.Categories.Add(category);
+        db.SaveChanges();
+        TempData["Success"] = "Category has been added successfully";
+        return RedirectToAction("Index");
+    }
+    #endregion
+
+    #region Edit
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if(id == null)
+        {
+            return NotFound();
+        }
+        var category = db.Categories.Find(id);
+        return View(category);
+    }
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if(!ModelState.IsValid) return View(obj);
+        db.Categories.Update(obj);
+        db.SaveChanges();
+        TempData["Success"] = "Category has been updated successfully";
+        return RedirectToAction("Index");
+    }
+    #endregion
+
+    #region Delete
+    public IActionResult Delete(int id) {
+    var category = db.Categories.Find(id);
+        if (category == null) return NotFound();
+        db.Categories.Remove(category);
+        db.SaveChanges();
+        TempData["Success"] = "Category has been deleted successfully";
+        return RedirectToAction("Index");
+    }
+    #endregion
+}
---
+++ Bulky/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Bulky.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opt):base(opt) {}
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Category>().HasData(
+        new Category {Id = 1,Name = "test1",DisplayOrder = 1},
+        new Category {Id = 2,Name = "test2",DisplayOrder = 2},
+        new Category {Id = 3,Name = "test3",DisplayOrder = 3});
+    }
+}
